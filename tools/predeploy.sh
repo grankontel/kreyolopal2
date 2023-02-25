7c86ac8f2755f5d77df309e123106b929a836fd7
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-02-25 10:09:00 +0100

    deploy script

---
+++ tools/predeploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No app provided"
+    exit 1
+fi
+
+path=`readlink -f "${BASH_SOURCE:-$0}"`
+DIR_PATH=`dirname $path`
+APP=$1
+APP_PATH=`readlink -f "${DIR_PATH}/../apps/${APP}"`
+BUILD_PATH=`readlink -f "${APP_PATH}/../../build/${APP}"`
+
+if not [ -d "$APP_PATH" ]; then
+    echo "$APP_PATH is not a directory."
+    exit 1
+fi
+
+echo "The current directory path is" $DIR_PATH
+echo "The app directory path is" $APP_PATH
+echo "The build directory path is" $BUILD_PATH
+
+mkdir -p ${BUILD_PATH}/.next/static
+
+cp ${APP_PATH}/package.json ${BUILD_PATH}/
+
+cd ${BUILD_PATH}/
+npm install --omit=dev
+
+cp -R ${APP_PATH}/public ${BUILD_PATH}/
+cp -R ${APP_PATH}/.next/standalone/ ${BUILD_PATH}/
+cp -R ${APP_PATH}/.next/static/ ${BUILD_PATH}/.next/static
+
