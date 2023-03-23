79722d20e91e8e3735e7a18c275f94b72ce234eb
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-03-23 21:09:52 +0100

    no container

--- libs/web-ui/src/layouts/StandardPage.js
+++ libs/web-ui/src/layouts/StandardPage.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { TopNavbar } from '../components/TopNavbar'
-import { Container, Content } from 'react-bulma-components'
+import { Content } from 'react-bulma-components'
 
 export function StandardPage({ children, getHead }) {
   return (
     <>
       {getHead()}
       <TopNavbar />
-      <Container className="main" renderAs="main">
+      <main className="main">
         {children}
-      </Container>
+      </main>
       <footer className="page-footer has-text-centered">
         <Content>&copy; TiMalo — 2022</Content>
       </footer>
