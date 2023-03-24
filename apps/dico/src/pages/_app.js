4d385562d26b046d31b71899b5da2d4060221acc
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-03-24 20:54:06 +0100

    added links to navbar

--- apps/dico/src/pages/_app.js
+++ apps/dico/src/pages/_app.js
@@ -2,9 +2,17 @@ import '@/styles/index.sass'
 import { StandardPage } from '@kreyolopal/web-ui'
 import DicoHead from '@/components/DicoHead'
 
+const links = [
+  {
+    id: 2,
+    url: '/contact',
+    text: 'Contact'
+  }
+]
+
 export default function App({ Component, pageProps }) {
   return (
-    <StandardPage getHead={() => <DicoHead />}>
+    <StandardPage links={links} getHead={() => <DicoHead />}>
       <Component {...pageProps} />
     </StandardPage>
   )
--- apps/front/src/pages/_app.js
+++ apps/front/src/pages/_app.js
@@ -2,9 +2,21 @@ import '@/styles/index.sass'
 import { StandardPage } from '@kreyolopal/web-ui'
 import FrontHead from '@/components/FrontHead'
 
+const links = [
+  {
+    id: 1,
+    url: process.env.DICO_URL,
+    text: 'Dictionnaire'
+  },
+  {
+    id: 2,
+    url: '/contact',
+    text: 'Contact'
+  }
+]
 export default function App({ Component, pageProps }) {
   return (
-    <StandardPage getHead={() => (<FrontHead />)}>
+    <StandardPage links={links} getHead={() => (<FrontHead />)}>
       <Component {...pageProps} />
     </StandardPage>
   )
