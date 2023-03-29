440170b41f2d65cecb636a2cc7cc712ec317a858
Thierry Malo <thierry.malo@ringover.com>
2023-03-29 16:42:06 +0200

    basic mongo connection

---
+++ tools/server/mongo2dic.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+
+const config = {
+  mongodb: {
+    // uri: process.env.MONGODB_URI,
+    host: process.env.MONGODB_HOST,
+    user: process.env.MONGODB_USER,
+    password: process.env.MONGODB_PASSWORD,
+    db: process.env.MONGODB_DB,
+    port: Number(process.env.MONGODB_PORT || 27017),
+  },
+}
+ 
+config.mongodb.uri = `mongodb://${config.mongodb.user}:${encodeURIComponent(
+  config.mongodb.password
+)}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`
+
+// Connection URL
+const client = new MongoClient(config.mongodb.uri);
+
+// Database Name
+const dbName = 'zakari';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('words');
+
+  // the following code examples can be pasted here...
+  const filter = {
+    entry: 'palaviré',
+  }
+  const projection = {
+    entry: 1,
+    variations: 1,
+    definitions: 1,
+  }
+
+  const cursor = collection.find(filter, { projection })
+  const result = await cursor.toArray()
+  //client.close()
+
+  const data = result.map((item) => {
+    return {
+      id: item._id,
+      entry: item.entry,
+      variations: item.variations,
+      definitions: item.definitions['gp'],
+    }
+  })
+
+  console.log(data)
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
