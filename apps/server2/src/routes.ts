98039984d2d7112ca22c381c095942f36a7b8980
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-12-28 09:11:03 +0100

    remove useless auth routes

--- apps/server2/src/routes.ts
+++ apps/server2/src/routes.ts
@@ -1,7 +1,6 @@
 import { cors } from 'hono/cors'
 import dicoRoutes from './api/dictionary'
 import wordsRoutes from "./api/words";
-import authRoutes from './api/auth/auth.routes';
 import { AppRouter } from './services/hono'
 import { winston_logger as logger } from './services/winston_logger'
 
@@ -14,8 +13,7 @@ function setRoutes( app:AppRouter ) {
   logger.info("Adding routes")
   app.route('/api/dictionary', dicoRoutes)
   app.route('/api/words', wordsRoutes)
-  app.route('/api/auth', authRoutes)
-
+ 
   return app
 }
 
