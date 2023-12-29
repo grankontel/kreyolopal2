3fcfabd7bfb5d77f55ed85162bd0c01679fd582c
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-12-29 13:50:22 +0100

    spellchecker

---
+++ apps/server2/src/api/spellcheck/facto.typo.ts
@@ -0,0 +1,12 @@
+// const Typo = require('typo-js')
+import Typo from 'typo-js';
+
+const createSpellchecker = (affix, dictionary) => {
+  const diko = new Typo('cpf_GP', affix.toString(), dictionary.toString())
+  return {
+    check: (word: string) => diko.check(word),
+    suggest: (word: string) => diko.suggest(word),
+  }
+}
+
+export default createSpellchecker
--- apps/server2/src/api/spellcheck/spellcheck.handlers.ts
+++ apps/server2/src/api/spellcheck/spellcheck.handlers.ts
@@ -1,11 +1,25 @@
 import { Context } from 'hono'
+import spellchecker from './lib.spellcheck'
 
 const postSpellCheck = async function (c: Context) {
   const logger = c.get('logger')
   const body = c.req.valid('json')
 
-  console.log(body)
-  return c.json({})
+  const lMessage = {
+    user: 1, // req.user.id,
+    tool: c.req.header('User-Agent'),
+    service: 'spellcheck',
+    kreyol: body.kreyol,
+    request:body.request.replace(/ç/, 's'),
+  }
+  console.log(lMessage)
+  return spellchecker
+  .check(lMessage)
+  .then(async (msg) => {
+	console.log(msg)
+	lMessage.response = msg
+	return c.json(lMessage, 200)
+  })
 }
 
 const postRating = async function (c: Context) {
