b5f2b2a7df40b174dada10b8f87eb765132a22cb
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-12-29 10:40:45 +0100

    corrected script

--- apps/server/scripts/update_dico.js
+++ apps/server/scripts/update_dico.js
@@ -38,7 +38,7 @@ const client = new S3Client(s3Options)
 // https://kreyol-dico.s3.amazonaws.com/dico/cpf_GP.aff
 
 const createPutCommand = (file, content) =>
-  new Promise((reject, resolve) => {
+  new Promise((resolve, reject) => {
     let data = ''
     try {
       data = content.toString('utf-8')
