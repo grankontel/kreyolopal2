43c0ad75565d09821c6fa88af1269e360d96f300
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-01-29 20:48:07 +0100

    add button

--- apps/dico/src/components/DicoEntry.jsx
+++ apps/dico/src/components/DicoEntry.jsx
@@ -1,18 +1,28 @@
-import { Heading } from 'react-bulma-components'
+import { Heading, Icon } from 'react-bulma-components'
+import { SignedIn } from '@clerk/nextjs'
 import AlsoList from './dictionnary/AlsoList'
 import SynonymList from './dictionnary/SynonymList'
 import Tranlations from './dictionnary/Translations'
 import VariationList from './dictionnary/VariationList'
 import UsageList from './dictionnary/UsageList'
+import FeatherIcon from './FeatherIcon'
 
 const DicoEntry = ({ item, kreyol, ...rest }) => {
     const nb_definitions = item.definitions.length
 
     return (
         <article className="dico_word" {...rest}>
-            <Heading size={3} renderAs="h2">
-                {item.entry}
-            </Heading>
+            <div className='is-flex button-addword'>
+                <SignedIn>
+                    <Icon>
+                        <FeatherIcon iconName="plus-square" />
+                    </Icon>
+                </SignedIn>
+                <Heading size={3} renderAs="h2" className='inline-flex' >
+                    {item.entry}
+                </Heading>
+
+            </div>
             {item.variations.length > 1 ? (
                 <VariationList variations={item.variations} />
             ) : null}
