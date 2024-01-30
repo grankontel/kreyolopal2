9f0202606ce54b3b7c691e454c1f420baaa83bed
Thierry Malo <thierry.malo@ringover.com>
2024-01-30 17:41:38 +0100

    me page

---
+++ apps/dico/src/pages/me/index.js
@@ -0,0 +1,47 @@
+import { Section } from "react-bulma-components";
+
+/*
+entry
+variations
+definition_no
+nature
+definition_fr
+usage
+synonyme
+confere
+
+  {
+    "entry": "débraké",
+    "variations": [
+      "débraké",
+      "débwaké"
+    ],
+    "definitions": {
+      "gp": [
+        {
+          "nature": [
+            "verbe"
+          ],
+          "meaning": {
+            "gp": "",
+            "fr": "contrebraquer."
+          },
+          "usage": [
+            "ou braké twòp, débraké tibwen avan  ou kyoulé."
+          ],
+          "synonyms": [],
+          "confer": [],
+          "quotes": []
+        }
+      ]
+    }
+  },
+*/
+
+export default function MePage() {
+	return (
+		<Section>
+			Me Page
+		</Section>
+	)
+}
