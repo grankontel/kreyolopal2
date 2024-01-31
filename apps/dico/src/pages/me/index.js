b49c5708728994e73a4f965b8fb6032944a95cb8
Thierry Malo <thierry.malo@ringover.com>
2024-01-31 15:59:37 +0100

    button onhover

--- apps/dico/src/pages/me/index.js
+++ apps/dico/src/pages/me/index.js
@@ -1,4 +1,8 @@
-import { Section } from "react-bulma-components";
+import { useState } from "react"
+import { Icon, Section, Table } from "react-bulma-components";
+import { FlagGp } from '@kreyolopal/web-ui'
+import FeatherIcon from "@/components/FeatherIcon";
+import classNames from "classnames";
 
 /*
 entry
@@ -39,9 +43,85 @@ confere
 */
 
 export default function MePage() {
-	return (
-		<Section>
-			Me Page
-		</Section>
-	)
+  const [isUsageHover, setUsageHover] = useState(false)
+  const usageClass = classNames({
+    'entry-add-button': true,
+    'line-hidden': !isUsageHover
+  })
+
+  return (
+    <Section>
+      Me Page
+
+      <Table size="fullwidth">
+        <thead>
+          <tr>
+            <th>
+              Entr&eacute;e
+            </th>
+            <th>
+              Kr&eacute;y&ograve;l
+            </th>
+            <th>
+              Variations
+            </th>
+            <th>
+              Nature
+            </th>
+            <th>
+              D&eacute;finition
+            </th>
+            <th>
+              D&eacute;finition (FR)
+            </th>
+            <th>
+              Usage
+            </th>
+            <th>
+              Synonyme
+            </th>
+            <th>
+              Voir aussi
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              d&eacute;brak&eacute;
+            </td>
+            <td>
+              <Icon>
+                <FlagGp />
+              </Icon>
+            </td>
+            <td>
+              <div>
+                d&eacute;brak&eacute;
+              </div>
+              <div>
+                d&eacute;bwak&eacute;
+              </div>
+            </td>
+            <td>
+              Vèwb
+            </td>
+            <td>
+
+            </td>
+            <td>
+              contrebraquer.
+            </td>
+            <td onMouseEnter={() => setUsageHover(true)} onMouseLeave={() => setUsageHover(false)}>
+              <div>ou braké twòp, débraké tibwen avan  ou kyoulé.</div>
+              <Icon className={usageClass}>
+                <FeatherIcon iconName="plus-square" />
+              </Icon>
+
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Section>
+  )
 }
