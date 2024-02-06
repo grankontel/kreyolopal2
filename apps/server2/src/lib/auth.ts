b92f4bbd29f373ebc3b244f81c2232ad5c846785
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-02-07 00:17:45 +0100

    auth lib

---
+++ apps/server2/src/lib/auth.ts
@@ -0,0 +1,25 @@
+import { Lucia } from 'lucia'
+import { adapter } from './db'
+import type { DatabaseUser } from './db'
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      username: attributes.username,
+    }
+  },
+})
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia
+    DatabaseUserAttributes: Omit<DatabaseUser, 'id'>
+  }
+}
+
+
