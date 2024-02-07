38466580fd05e21e9f48fdca8c15dffa6a7a613c
Thierry Malo <thierry.malo@ringover.com>
2024-02-07 20:10:30 +0100

    improved csrf middleware

--- apps/server2/src/middlewares/csrf.ts
+++ apps/server2/src/middlewares/csrf.ts
@@ -1,5 +1,45 @@
-import { verifyRequestOrigin } from 'lucia'
+// import { verifyRequestOrigin } from 'lucia'
 import type { MiddlewareHandler } from 'hono'
+import { winston_logger } from '#services/winston_logger'
+
+function domain_from_url(url) {
+  var result
+  var match
+  if (match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im)) {
+      result = match[1]
+      if (match = result.match(/^[^\.]+\.(.+\..+)$/)) {
+          result = match[1]
+      }
+  }
+  return result
+}
+
+export function verifyRequestOrigin(origin: string, allowedDomains: string[]): boolean {
+	if (!origin || allowedDomains.length === 0) return false;
+	const originHost = safeURL(origin)?.host ?? null;
+
+  if (!originHost) return false;
+	for (const domain of allowedDomains) {
+		let host: string | null;
+		if (domain.startsWith("http://") || domain.startsWith("https://")) {
+			host = safeURL(domain)?.host ?? null;
+
+    } else {
+			host = safeURL("https://" + domain)?.host ?? null;
+		}
+    console.log({originDomain: domain_from_url(originHost), hostDomain: domain_from_url(originHost)})
+		if (domain_from_url(originHost) === domain_from_url(host)) return true;
+	}
+	return false;
+}
+
+function safeURL(url: URL | string): URL | null {
+	try {
+		return new URL(url);
+	} catch {
+		return null;
+	}
+}
 
 export const csrfMiddleware = (): MiddlewareHandler => {
   return async (c, next) => {
@@ -15,6 +55,7 @@ export const csrfMiddleware = (): MiddlewareHandler => {
       !hostHeader ||
       !verifyRequestOrigin(originHeader, [hostHeader])
     ) {
+      winston_logger.warning(`invalid request origin from host: ${hostHeader}, origin: ${originHeader}`)
       return c.body(null, 403)
     }
     return next()
