f03531d36612969641bd22ea20df9b13ff7e3b26
Thierry Malo <thierry.malo@ringover.com>
2024-02-09 14:57:48 +0100

    create layout

---
+++ apps/dico/src/layouts/Standard.jsx
@@ -0,0 +1,44 @@
+import { StandardPage } from '@kreyolopal/web-ui'
+import DicoHead from '@/components/DicoHead'
+import { useAuth } from '@/AuthContext'
+import { useEffect } from 'react'
+
+const dico_url = process.env.NEXT_PUBLIC_DICO_URL || `http://localhost:${process.env.PORT || 3000}`
+const links = [
+	{
+		id: 1,
+		url: dico_url,
+		text: 'Dictionnaire',
+	},
+	{
+		id: 2,
+		url: `${dico_url}/spellcheck`,
+		text: 'Correcteur',
+	},
+	{
+		id: 3,
+		url: '/contact',
+		text: 'Contact',
+	},
+]
+
+export default function Standard({ children }) {
+	let auth = useAuth()
+	console.log(auth)
+	
+	return (
+		<StandardPage
+			links={links}
+			getHead={() => <DicoHead />}
+			CustomItems={() => (
+				<>
+					<span className="navbar-item">
+						Connect
+					</span>
+				</>
+			)}
+		>
+			{children}
+		</StandardPage>
+	)
+}
