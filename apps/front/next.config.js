19fd726cf5871d258454c2b39a7639e1a64072a4
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-02-10 17:26:32 +0100

    upgrade to 13.5.6

--- apps/front/next.config.js
+++ apps/front/next.config.js
@@ -4,18 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   poweredByHeader: false, 
-  async rewrites() {
-    return {
-      // After checking all Next.js pages (including dynamic routes)
-      // and static files we proxy any other requests
-      fallback: [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.API_SERVER}/api/:path*`,
-        },
-      ],
-    }
-  },
+
 }
 
 module.exports = nextConfig
