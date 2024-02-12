1b9cd0a77bcad68a9cccd10eccaa7061be555445
Thierry Malo <thierry.malo@ringover.com>
2024-02-12 14:31:44 +0100

    create admin site

---
+++ apps/admin/next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@kreyolopal/web-ui'],
+  reactStrictMode: true,
+  output: 'standalone',
+  poweredByHeader: false,
+  async rewrites() {
+    return {
+      // After checking all Next.js pages (including dynamic routes)
+      // and static files we proxy any other requests
+      fallback: [
+        {
+          source: '/postgrest/:path*',
+          destination: `${process.env.POSTGREST_SERVER}/:path*`,
+        },
+      ],
+    }
+  },
+}
+
+module.exports = nextConfig
