ae65b197154b7bce3d70ccdc21338c3fb20d8376
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-02-15 23:13:54 +0100

    prettier again

--- libs/web-admin/src/components/admin/Auth_userEdit.js
+++ libs/web-admin/src/components/admin/Auth_userEdit.js
@@ -1,20 +1,26 @@
-import { BooleanInput, DateTimeInput, Edit, SimpleForm, TextInput } from 'react-admin';
+import {
+  BooleanInput,
+  DateTimeInput,
+  Edit,
+  SimpleForm,
+  TextInput,
+} from 'react-admin'
 
 export const Auth_userEdit = () => (
-    <Edit>
-        <SimpleForm>
-            <TextInput disabled source="id"  />
-            <TextInput source="username" />
-            <TextInput source="firstname" />
-            <TextInput source="lastname" />
-            <TextInput source="email" />
-            <TextInput source="password" />
-            <TextInput source="lastlogin" />
-            <TextInput source="email_verif_token" />
-            <TextInput source="reset_pwd_token" />
-            <BooleanInput source="is_admin" />
-            <DateTimeInput disabled source="created_at" />
-            <DateTimeInput disabled source="updated_at" />
-        </SimpleForm>
-    </Edit>
-);
+  <Edit>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <TextInput source="username" />
+      <TextInput source="firstname" />
+      <TextInput source="lastname" />
+      <TextInput source="email" />
+      <TextInput source="password" />
+      <TextInput source="lastlogin" />
+      <TextInput source="email_verif_token" />
+      <TextInput source="reset_pwd_token" />
+      <BooleanInput source="is_admin" />
+      <DateTimeInput disabled source="created_at" />
+      <DateTimeInput disabled source="updated_at" />
+    </SimpleForm>
+  </Edit>
+)
