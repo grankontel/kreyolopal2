3a92ae447637f5651efd9e0709e610275c5b0f5b
Thierry Malo <thierry.malo@ringover.com>
2024-02-15 19:34:24 +0100

    useless admin page

--- apps/dico/src/pages/admin/index.js
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-const AdminApp = dynamic(() => import("@/components/AdminApp"), { ssr: false });
-
-const Admin = () => {
-    return <AdminApp />;
-};
-
-export default Admin;
