eb60ad5af4d538f4df3625f99524530aa63b1957
Thierry Malo <thierry.malo@ringover.com>
2024-02-16 15:01:03 +0100

    verify routes

--- apps/server2/src/api/verify/verify.handlers.ts
+++ apps/server2/src/api/verify/verify.handlers.ts
@@ -4,6 +4,9 @@ import { DatabaseUser, pgPool } from '#lib/db'
 import { logUserIn } from '#utils/apiHelpers'
 import { createHttpException } from '#utils/createHttpException'
 import { argon2 } from '#utils/argon'
+import { authService } from '#services/authService'
+import { getResetpwd } from '@kreyolopal/mails'
+import { sendEmail } from '#services/mail'
 
 const verifyMail = async function (c: Context) {
   const logger = c.get('logger')
@@ -95,7 +98,59 @@ const postResetPwd = async function (c: Context) {
   const logger = c.get('logger')
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  const body = c.req.valid('json') as Record<string, string>
+  const { email } = c.req.valid('json') as Record<string, string>
+  const url = new URL(c.req.url)
+  const origin = `${url.protocol}://${url.host}`
+
+  const newToken = authService.generateVerifToken(email)
+  return pgPool
+    .query(
+      `UPDATE auth_user 
+		SET email_verif_token = $1 
+		WHERE email = $2 
+		RETURNING id, firstname, lastname, email`,
+      [newToken, email]
+    )
+    .then((result) => {
+      if (!result?.rowCount)
+        return c.json(
+          {
+            status: 'success',
+            data: {},
+          },
+          200
+        )
+
+      logger.info('sending reset pwd mail')
+      const retrievedUser = result.rows[0]
+
+      const templateData = {
+        user: {
+          id: retrievedUser.id,
+          firstname: retrievedUser.firstname,
+          lastname: retrievedUser.lastname,
+          email: retrievedUser.email,
+        },
+        confirm_url: `${origin}/resetpwd/${newToken}`,
+      }
+
+      return sendEmail(
+        getResetpwd,
+        templateData,
+        'Chanjé modpas',
+        `'${retrievedUser.firstname} ${retrievedUser.lastname}' <${retrievedUser.email}>`
+      ).then(() => {
+        logger.info('Just sent mail')
+
+        return c.json(
+          {
+            status: 'success',
+            data: {},
+          },
+          200
+        )
+      })
+    })
 }
 
 export default { verifyMail, postResetPwdToken, postResetPwd }
