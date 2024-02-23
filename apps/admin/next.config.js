18d0e7544f84fc3158590e2e7f6234495ca57867
Thierry Malo <thierry.malo@ringover.com>
2024-02-24 00:05:48 +0100

    make admin deployable

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,3 +1,5 @@
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //  transpilePackages: ['@kreyolopal/web-ui'],
@@ -22,4 +24,32 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    console.log('***************')
+    return {
+      /* development only config options here */
+      reactStrictMode: true,
+      output: 'standalone',
+      poweredByHeader: false,
+      async rewrites() {
+        return {
+          // After checking all Next.js pages (including dynamic routes)
+          // and static files we proxy any other requests
+          fallback: [
+            {
+              source: '/api/:path*',
+              destination: 'http://__API_PATH__',
+            },
+            {
+              source: '/postgrest/:path*',
+              destination: 'http://__POSTGREST_PATH__',
+            },
+          ],
+        }
+      },
+    }
+  }
+
+  return nextConfig
+}
