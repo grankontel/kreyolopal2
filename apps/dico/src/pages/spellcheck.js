f30ada8a088e66f0e6ccfe70a53539e095d75a26
Thierry Malo <thierry.malo@ringover.com>
2024-02-28 10:40:56 +0100

    failsafe usage of env variables

--- apps/dico/src/pages/spellcheck.js
+++ apps/dico/src/pages/spellcheck.js
@@ -263,8 +263,9 @@ export const config = {
 }
 
 export async function getServerSideProps(context) {
+  const cookieName = process.env.NEXT_PUBLIC_COOKIE_NAME || 'wabap'
   const session = parseCookie(
-    context.req.cookies?.[process.env.NEXT_PUBLIC_COOKIE_NAME]
+    context.req.cookies?.[cookieName]
   )
   if (!session) {
     return {
