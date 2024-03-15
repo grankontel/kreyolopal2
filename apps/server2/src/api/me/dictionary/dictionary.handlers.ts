58ddda7119576134b198935d076094c090094d96
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-03-15 09:57:04 -0400

    chore

--- apps/server2/src/api/me/dictionary/dictionary.handlers.ts
+++ apps/server2/src/api/me/dictionary/dictionary.handlers.ts
@@ -3,6 +3,20 @@ import config from '#config'
 import { createHttpException } from '#utils/createHttpException'
 import { WordsRepository } from '#lib/words.repository'
 
+function formatDate(date) {
+  if (date === null) return null
+
+  let d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear()
+
+  if (month.length < 2) month = '0' + month
+  if (day.length < 2) day = '0' + day
+
+  return [year, month, day].join('-')
+}
+
 const getWord = async function (c: Context) {
   const logger = c.get('logger')
   const client = c.get('mongodb')
@@ -67,20 +81,6 @@ const getWord = async function (c: Context) {
       statusText: 'Unknown error.',
     })
   }
-}
-
-function formatDate(date) {
-  if (date === null) return null
-
-  let d = new Date(date),
-    month = '' + (d.getMonth() + 1),
-    day = '' + d.getDate(),
-    year = d.getFullYear()
-
-  if (month.length < 2) month = '0' + month
-  if (day.length < 2) day = '0' + day
-
-  return [year, month, day].join('-')
 }
 
 const bookmarkWord = async function (c: Context) {
