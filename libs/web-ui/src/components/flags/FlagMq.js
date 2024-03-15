d27c380ab63ca24b8fac8ed08f4a96e4c5e8019b
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-03-15 09:32:23 -0400

    extra flags

---
+++ libs/web-ui/src/components/flags/FlagMq.js
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+export const FlagMq = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 800 400" {...props}>
+    <path fill="#231f1e" d="M0 0h800v400H0z" />
+    <path fill="#00a650" d="M0 0h800v200H0z" />
+    <path fill="#ef1923" d="m0 0 267 200L0 400z" />
+  </svg>
+)
--- libs/web-ui/src/index.js
+++ libs/web-ui/src/index.js
@@ -1,3 +1,4 @@
+
 export { TopNavbar } from './components/TopNavbar'
 export { HeroSearchBox } from './components/HeroSearchBox'
 export { findByType, findAllByType } from './components/FindByType'
@@ -15,7 +16,10 @@ export { LoginForm } from './forms/LoginForm'
 export { SignupForm } from './forms/SignupForm'
 
 // flags
-export { FlagGp } from './components/FlagGp'
+export { FlagGp } from './components/flags/FlagGp'
+export { FlagMq } from './components/flags/FlagMq'
+export { FlagHt } from './components/flags/FlagHt'
+export { KreyolFlag } from './components/flags/KreyolFlag'
 
 // Layouts
 export { StandardPage } from './layouts/StandardPage'
