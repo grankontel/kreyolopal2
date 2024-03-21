51d2577324424af7434bfe059fec08b454d4057e
Thierry Malo <thierry.malo@ringover.com>
2024-03-21 10:05:07 +0100

    allow headers

--- apps/server2/src/api/me/dictionary/dictionary.handlers.ts
+++ apps/server2/src/api/me/dictionary/dictionary.handlers.ts
@@ -427,7 +427,10 @@ const listWords = async function (c: Context) {
     if (data.length > 0) {
       const nb = await client.db(config.mongodb.db).collection('personal').countDocuments(filter)
       console.log(nb)
+      var endRange = Math.min(nb, offset + limit)
       c.res.headers.append('Cache-Control', 'private, maxage=86400')
+      c.res.headers.append('Access-Control-Expose-Headers', 'X-Total-Count, Content-Range');
+      c.res.headers.append('Content-Range', `${offset}-${endRange}/${nb}`)
       c.res.headers.append('X-Total-Count', nb)
 
       c.status(200)
