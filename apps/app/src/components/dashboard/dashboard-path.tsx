e67210eed52482b3b9e20afbb917f67fa34fe06f
Thierry Malo <thierry.malo@ringover.com>
2024-03-23 22:39:27 +0100

    prettier

--- apps/app/src/components/dashboard/dashboard-path.tsx
+++ apps/app/src/components/dashboard/dashboard-path.tsx
@@ -8,21 +8,31 @@ import {
   Breadcrumb,
 } from '@/components/ui/breadcrumb'
 import { DashboardMenuItem } from '@/lib/dashboard'
-import { hashKey } from '@/lib/utils';
-import { useDicoStore } from '@/store/dico-store';
-import { usePathname } from 'next/navigation';
-import { Fragment } from 'react';
+import { hashKey } from '@/lib/utils'
+import { useDicoStore } from '@/store/dico-store'
+import { usePathname } from 'next/navigation'
+import { Fragment } from 'react'
 
-const getBreadcrumb = (pathName: string, menus: Array<DashboardMenuItem>, start: Array<{ label: string, path: string }> = []): Array<{ label: string, path: string }> | boolean => {
-  const sortedMenus = menus.toSorted((a, b) => (a.path?.length || 0) - (b.path?.length || 0))
+const getBreadcrumb = (
+  pathName: string,
+  menus: Array<DashboardMenuItem>,
+  start: Array<{ label: string; path: string }> = []
+): Array<{ label: string; path: string }> | boolean => {
+  const sortedMenus = menus.toSorted(
+    (a, b) => (a.path?.length || 0) - (b.path?.length || 0)
+  )
 
   for (let index = 0; index < sortedMenus.length; index++) {
-    const menu = sortedMenus[index];
+    const menu = sortedMenus[index]
     if (menu.path != null && pathName.includes(menu.path as string)) {
       return [...start, { label: menu.label, path: menu.path }]
     }
     if (menu.items) {
-      const result: Array<{ label: string, path: string }> | boolean = getBreadcrumb(pathName, menu.items, [...start, { label: menu.label, path: menu.path as string }])
+      const result: Array<{ label: string; path: string }> | boolean = getBreadcrumb(
+        pathName,
+        menu.items,
+        [...start, { label: menu.label, path: menu.path as string }]
+      )
       if (result) return result
     }
   }
@@ -34,7 +44,7 @@ export default function DashboardPath() {
   const pathName = usePathname()
   const { menus } = useDicoStore()
 
-  const crumbs = getBreadcrumb(pathName, menus) as { label: string, path: string }[]
+  const crumbs = getBreadcrumb(pathName, menus) as { label: string; path: string }[]
 
   return (
     <Breadcrumb className="breadcrumb">
@@ -43,12 +53,10 @@ export default function DashboardPath() {
           <Fragment key={hashKey('crumb', crumb.label)}>
             <BreadcrumbItem>
               <BreadcrumbLink href={crumb.path}>{crumb.label}</BreadcrumbLink>
-
             </BreadcrumbItem>
-            {(index < crumbs.length - 1) ? (<BreadcrumbSeparator />) : (' ')}
+            {index < crumbs.length - 1 ? <BreadcrumbSeparator /> : ' '}
           </Fragment>
         ))}
-
       </BreadcrumbList>
     </Breadcrumb>
   )
--- apps/app/src/components/dashboard/side-menu.tsx
+++ apps/app/src/components/dashboard/side-menu.tsx
@@ -6,32 +6,37 @@ import FeatherIcon from '../FeatherIcon'
 import { useDicoStore } from '@/store/dico-store'
 import { DashboardMenuItem } from '@/lib/dashboard'
 
-const SideMenuItem = ({ menu, active = false }: { menu: DashboardMenuItem, active: boolean }) => {
-  let cl = active ? "flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
-    : "flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+const SideMenuItem = ({
+  menu,
+  active = false,
+}: {
+  menu: DashboardMenuItem
+  active: boolean
+}) => {
+  let cl = active
+    ? 'flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50'
+    : 'flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50'
 
-  cl = menu.items?.length ? cl = cl + ' uppercase text-xs' : cl
+  cl = menu.items?.length ? (cl = cl + ' uppercase text-xs') : cl
   return (
     <li>
-      <Link
-        className={cl} href={menu.path || '#'}
-      >
-        {menu.icon ? (<FeatherIcon iconName={menu.icon} className="h-4 w-4" />) : ''}
+      <Link className={cl} href={menu.path || '#'}>
+        {menu.icon ? <FeatherIcon iconName={menu.icon} className="h-4 w-4" /> : ''}
         {menu.label}
       </Link>
 
       {menu.items?.length ? (
-        <ul className='pl-4'>
+        <ul className="pl-4">
           {menu.items.map((item) => (
             <SideMenuItem menu={item} key={hashKey('menu', item.label)} />
           ))}
         </ul>
-      ) : ''}
-
+      ) : (
+        ''
+      )}
     </li>
   )
 }
-
 
 export default function SideMenu() {
   const { menus } = useDicoStore()
@@ -40,7 +45,9 @@ export default function SideMenu() {
       <ul>
         {menus.map((menu) => {
           const active = false
-          return (<SideMenuItem key={hashKey('menu', menu.label)} menu={menu} active={active} />)
+          return (
+            <SideMenuItem key={hashKey('menu', menu.label)} menu={menu} active={active} />
+          )
         })}
       </ul>
     </nav>
