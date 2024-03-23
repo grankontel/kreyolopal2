68ad575f178d547ff57a35ad10d6642070b0d78e
Thierry Malo <thierry.malo@ringover.com>
2024-03-23 22:37:21 +0100

    side menu

--- apps/app/src/components/dashboard/side-menu.tsx
+++ apps/app/src/components/dashboard/side-menu.tsx
@@ -1,140 +1,47 @@
-import { IconAttributes } from '@/lib/utils'
+'use client'
+
+import { hashKey } from '@/lib/utils'
 import Link from 'next/link'
+import FeatherIcon from '../FeatherIcon'
+import { useDicoStore } from '@/store/dico-store'
+import { DashboardMenuItem } from '@/lib/dashboard'
 
-function HomeIcon(props: IconAttributes) {
+const SideMenuItem = ({ menu, active = false }: { menu: DashboardMenuItem, active: boolean }) => {
+  let cl = active ? "flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
+    : "flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+
+  cl = menu.items?.length ? cl = cl + ' uppercase text-xs' : cl
   return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-      <polyline points="9 22 9 12 15 12 15 22" />
-    </svg>
+    <li>
+      <Link
+        className={cl} href={menu.path || '#'}
+      >
+        {menu.icon ? (<FeatherIcon iconName={menu.icon} className="h-4 w-4" />) : ''}
+        {menu.label}
+      </Link>
+
+      {menu.items?.length ? (
+        <ul className='pl-4'>
+          {menu.items.map((item) => (
+            <SideMenuItem menu={item} key={hashKey('menu', item.label)} />
+          ))}
+        </ul>
+      ) : ''}
+
+    </li>
   )
 }
 
-function PackageIcon(props: IconAttributes) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m7.5 4.27 9 5.15" />
-      <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
-      <path d="m3.3 7 8.7 5 8.7-5" />
-      <path d="M12 22V12" />
-    </svg>
-  )
-}
-
-function UsersIcon(props: IconAttributes) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-      <circle cx="9" cy="7" r="4" />
-      <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-      <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-    </svg>
-  )
-}
-
-function LineChartIcon(props: IconAttributes) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M3 3v18h18" />
-      <path d="m19 9-5 5-4-4-3 3" />
-    </svg>
-  )
-}
 
 export default function SideMenu() {
+  const { menus } = useDicoStore()
   return (
     <nav className="menu grid items-start px-4 text-sm font-medium">
       <ul>
-        <li>
-          <Link
-            className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
-            href="#"
-          >
-            <HomeIcon className="h-4 w-4" />
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-            href="#"
-          >
-            <PackageIcon className="h-4 w-4" />
-            Products
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-            href="#"
-          >
-            <UsersIcon className="h-4 w-4" />
-            Customers
-          </Link>
-          <ul className="pl-3">
-            <li>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="#"
-              >
-                Newest
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <Link
-            className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-            href="#"
-          >
-            <LineChartIcon className="h-4 w-4" />
-            Analytics
-          </Link>
-        </li>
+        {menus.map((menu) => {
+          const active = false
+          return (<SideMenuItem key={hashKey('menu', menu.label)} menu={menu} active={active} />)
+        })}
       </ul>
     </nav>
   )
