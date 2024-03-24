9ad86327038fea3d8dc74e6b30b71eee3db49920
Thierry Malo <3168322+tmalo@users.noreply.github.com>
2024-03-24 11:22:25 +0100

    Update esbuild.config.js

---
+++ libs/react-ui/esbuild.config.js
@@ -0,0 +1,11 @@
+module.exports={
+		entryPoints: ['./src/index.ts'],
+		outfile: 'dist/index.js',
+		bundle: true,
+		minify: true,
+		treeShaking: true,
+		platform: 'node',
+		format: 'cjs',
+		target: 'node20',
+		plugins: [nodeExternalsPlugin(), sassPlugin()],
+	}
