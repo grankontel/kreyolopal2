0c63357d29c5b1c8958fd769527925a8ff1a6afe
Thierry Malo <thierry.malo@ringover.com>
2024-03-27 13:49:10 +0100

    entry bookmark

--- apps/app/src/components/entry.tsx
+++ apps/app/src/components/entry.tsx
@@ -3,11 +3,11 @@
  * @see https://v0.dev/t/7Zyk89nt9tB
  */
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
 import { KreyolLanguage, IconAttributes } from '@kreyolopal/react-ui'
 import { DictionaryEntry, UserDictionaryEntry } from '@/lib/types'
 import { hashKey, onlyUnique } from '@/lib/utils'
 import { EntryDefinition } from './entry-definition'
+import { EntryBookmarkButton } from './entry-bookmark-button'
 
 export function Entry({
   kreyol,
@@ -19,8 +19,9 @@ export function Entry({
   value: UserDictionaryEntry
   dicoUrl: (word: string) => string
 }) {
-
-  const source: DictionaryEntry = (value.is_bookmarked ? value.bookmark : value.entry) as DictionaryEntry
+  const source: DictionaryEntry = (
+    value.is_bookmarked ? value.bookmark : value.entry
+  ) as DictionaryEntry
 
   const relatedList = [source]
     .map((entry) => {
@@ -39,7 +40,6 @@ export function Entry({
     .flat()
     .filter(onlyUnique)
 
-
   return (
     <div className="flex flex-col min-h-screen" {...props}>
       <main className="flex-1 py-6">
@@ -47,9 +47,7 @@ export function Entry({
           <div className="grid items-start gap-2">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
               <span className="flex items-center gap-2">
-                <Button size="icon" variant="outline">
-                  <BookmarkIcon className="h-6 w-6" />
-                </Button>
+                <EntryBookmarkButton entry={source.entry} bookmarked={value.is_bookmarked} />
                 {source.entry}
               </span>
             </h1>
@@ -97,6 +95,26 @@ export function Entry({
         </div>
       </main>
     </div>
+  )
+}
+
+function BookmarkedIcon(props: IconAttributes) {
+  return (
+    <svg
+      {...props}
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.5 2C3.22386 2 3 2.22386 3 2.5V13.5C3 13.6818 3.09864 13.8492 3.25762 13.9373C3.41659 14.0254 3.61087 14.0203 3.765 13.924L7.5 11.5896L11.235 13.924C11.3891 14.0203 11.5834 14.0254 11.7424 13.9373C11.9014 13.8492 12 13.6818 12 13.5V2.5C12 2.22386 11.7761 2 11.5 2H3.5Z"
+        fill="currentColor"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
   )
 }
 
