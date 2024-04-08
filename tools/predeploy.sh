18acf9a9e7d1bef313e4449b46d53d745d28b6d2
Thierry Malo <thierry.malo@ringover.com>
2024-04-08 11:00:06 +0200

    finetune predeploy

--- tools/predeploy.sh
+++ tools/predeploy.sh
@@ -2,8 +2,8 @@
 
 path=`realpath "${BASH_SOURCE:-$0}"`
 DIR_PATH=`dirname $path`
-FRONT_PATH=`realpath "${DIR_PATH}/../apps/front"`
-ADMIN_PATH=`realpath "${DIR_PATH}/../apps/admin"`
+# FRONT_PATH=`realpath "${DIR_PATH}/../apps/front"`
+# ADMIN_PATH=`realpath "${DIR_PATH}/../apps/admin"`
 SRV2_PATH=`realpath "${DIR_PATH}/../apps/server2"`
 BUILD_PATH="${DIR_PATH}/../build"
 
@@ -14,28 +14,28 @@ fi
 mkdir -p ${BUILD_PATH}/data/db
 mkdir -p ${BUILD_PATH}/data/mongodb
 
-${DIR_PATH}/cpnext.sh dico
-${DIR_PATH}/cpnext.sh admin
-${DIR_PATH}/cpdist.sh server
+# ${DIR_PATH}/cpnext.sh dico
+# ${DIR_PATH}/cpnext.sh admin
+# ${DIR_PATH}/cpdist.sh server
 ${DIR_PATH}/cpdist.sh server2
 
 cd  ${BUILD_PATH}/dico
 NODE_ENV='production' npm install --omit=dev --package-lock-only
 
-cp -R ${ADMIN_PATH}/prisma ${BUILD_PATH}/admin/
+# cp -R ${ADMIN_PATH}/prisma ${BUILD_PATH}/admin/
 
-cd  ${BUILD_PATH}/admin
-NODE_ENV='production' npm install --omit=dev --package-lock-only
-sed -i -e 's!"http://__API_PATH__"!(process.env.API_SERVER || "") + "/api/:path*"!g' ./server.js
-sed -i -e 's!"http://__POSTGREST_PATH__"!(process.env.POSTGREST_SERVER || "") + "/:path*"!g' ./server.js
+# cd  ${BUILD_PATH}/admin
+# NODE_ENV='production' npm install --omit=dev --package-lock-only
+# sed -i -e 's!"http://__API_PATH__"!(process.env.API_SERVER || "") + "/api/:path*"!g' ./server.js
+# sed -i -e 's!"http://__POSTGREST_PATH__"!(process.env.POSTGREST_SERVER || "") + "/:path*"!g' ./server.js
 
-cd  ${BUILD_PATH}/server
-NODE_ENV='production' npm install --omit=dev --package-lock-only
+# cd  ${BUILD_PATH}/server
+# NODE_ENV='production' npm install --omit=dev --package-lock-only
 
 cd  ${BUILD_PATH}/server2
 NODE_ENV='production' npm install --omit=dev --package-lock-only
 
-cp -R ${FRONT_PATH}/build ${BUILD_PATH}/www/
+# cp -R ${FRONT_PATH}/build ${BUILD_PATH}/www/
 
 # cp -R ${SRV2_PATH}/build ${BUILD_PATH}/server2/
 # cp -R ${SRV2_PATH}/package.json ${BUILD_PATH}/server2/package.json
