9b8cdb0daed16e1aebf2df5234818cca2db6f172
Thierry Malo <thierry.malo@ringover.com>
2024-04-12 08:27:20 +0200

    get lexicon

--- apps/dico/src/app/dashboard/lexicons/[username]/[slug]/page.tsx
+++ apps/dico/src/app/dashboard/lexicons/[username]/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { notFound } from 'next/navigation'
 import { isLoggedIn } from '@/app/dashboard/is-logged-in'
+import { getLexicon } from '@/queries/lexicons/get-lexicon';
+import MainPanel from '@/components/dashboard/main-panel';
 
 export const runtime = 'edge'
 
@@ -13,14 +15,17 @@ export default async function Page({
     return undefined
   }
 
-  const data = true
+  const data = await getLexicon(params.username, params.slug)
 
   if (!data) {
     return notFound()
   }
   return (
     <div>
+      <MainPanel title={data.name} description={data.description}>
       {params.username} / {params.slug}
+
+      </MainPanel>
     </div>
   )
 }
--- apps/dico/src/components/dashboard/main-panel.tsx
+++ apps/dico/src/components/dashboard/main-panel.tsx
@@ -1,14 +1,21 @@
+interface MainPanelProps {
+  title: string
+  description?: string
+  children: React.ReactNode
+}
 export default function MainPanel({
   title,
+  description,
   children,
-}: Readonly<{
-  title: string
-  children: React.ReactNode
-}>) {
+}: Readonly<MainPanelProps>) {
   return (
     <div>
-      <section className="flex p-6 bg-gray-100 border-y-gray-200 border-y-2 dark:bg-gray-800 dark:border-y-gray-700">
+      <section className=" p-6 bg-gray-100 border-y-gray-200 border-y-2 dark:bg-gray-800 dark:border-y-gray-700">
         <h1 className="font-bold text-2xl">{title}</h1>
+        {description === undefined ? (' ') : (
+          <p className="text-sm text-muted-foreground">
+            {description}
+          </p>)}
       </section>
       <main className="flex-1 p-4 md:p-6">{children}</main>
     </div>
