51ea9ee4e18a5eb0297951d01a8b5b87dfece5c5
Thierry Malo <thierry.malo@ringover.com>
2024-04-15 16:00:03 +0200

    improve form

--- apps/dico/src/app/dashboard/dictionary/proposal/[entry]/page.tsx
+++ apps/dico/src/app/dashboard/dictionary/proposal/[entry]/page.tsx
@@ -18,38 +18,59 @@ export default async function Page({ params }: { params: { entry: string } }) {
     <div className="flex flex-col min-h-screen">
       <main className="flex-1 py-6">
         <div className="container space-y-6 px-4 md:px-6">
-          <div className="grid items-start gap-2">
+          <div className="grid  items-start gap-2">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
               <span className="flex items-center gap-2">
                 <BookmarkIcon className="h-6 w-6" />
                 {entry}
               </span>
             </h1>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4 text-gray-500 dark:text-gray-400">
+
+            <div className="grid gap-2 p-4 width-4/5 mx-auto border border-radius border-gray-200 dark:border-gray-700 border-solid">
+              <div className="grid grid-cols-5 items-center gap-4 text-gray-500 dark:text-gray-400">
                 <Label htmlFor="variations" className="text-left">
                   Variations
                 </Label>
                 <Input
-                  className="col-span-3"
+                  className="col-span-4"
                   type="text"
                   name="variations"
                   placeholder={`entrez les différentes façon d'écrire ${entry}`}
                 />
               </div>
 
-              <div className="grid grid-cols-4 items-center gap-4">
+              <div className="grid grid-cols-5 items-center gap-4">
                 <Label htmlFor="kreyol" className="text-left">
                   Kreyol
                 </Label>
                 <KreyolCombobox />
               </div>
 
-              <div className="grid grid-cols-4 items-center gap-4">
+              <div className="grid grid-cols-5 items-center gap-4">
                 <Label htmlFor="nature" className="text-left">
                   Nature
                 </Label>
-                <NatureCombobox  />
+                <NatureCombobox />
+              </div>
+
+              <div className="grid grid-cols-5 items-center gap-4">
+
+                <Label htmlFor='meaning' className='text-left'>
+                  Définition
+                </Label>
+                <div className="col-span-4"
+                >
+<div className="grid grid-cols-4 items-center gap-1"> 
+
+                  <KreyolCombobox />
+                  <Input
+                    className="col-span-3"
+                    type="text"
+                    name="meaning"
+                    placeholder="Entrez la définition de l'entrée"
+                  />
+</div>
+                </div>
               </div>
 
             </div>
