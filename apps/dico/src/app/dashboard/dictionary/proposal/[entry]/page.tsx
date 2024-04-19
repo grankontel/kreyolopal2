4244789a34f90556cf5bd9e24e1cce16aa8dd2a1
Thierry Malo <thierry.malo@ringover.com>
2024-04-19 08:49:46 +0200

    tags for synonym

--- apps/dico/src/app/dashboard/dictionary/proposal/[entry]/page.tsx
+++ apps/dico/src/app/dashboard/dictionary/proposal/[entry]/page.tsx
@@ -1,8 +1,10 @@
 import { isLoggedIn } from '@/app/dashboard/is-logged-in'
 import FeatherIcon from '@/components/FeatherIcon'
 import { BookmarkIcon } from '@/components/bookmark-icon'
+import { AddEntry } from '@/components/forms/add-entry'
 import { LanguageCombobox } from '@/components/language-combobox'
 import { NatureCombobox } from '@/components/nature-combobox'
+import { Tags } from '@/components/tags'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -15,7 +17,6 @@ export default async function Page({ params }: { params: { entry: string } }) {
     return undefined
   }
   const { entry } = params
-
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-1 py-6">
@@ -28,85 +29,7 @@ export default async function Page({ params }: { params: { entry: string } }) {
               </span>
             </h1>
 
-            <div className="grid gap-2 p-4 width-4/5 mx-auto border border-radius border-gray-200 dark:border-gray-700 border-solid">
-              <div className="grid grid-cols-5 items-center gap-4 text-gray-500 dark:text-gray-400">
-                <Label htmlFor="variations" className="text-left">
-                  Variations
-                </Label>
-                <Input
-                  className="col-span-4"
-                  type="text"
-                  name="variations"
-                  placeholder={`entrez les différentes façon d'écrire ${entry}`}
-                />
-              </div>
-
-              <div className="grid grid-cols-5 items-center gap-4">
-                <Label htmlFor="kreyol" className="text-left">
-                  Kreyol
-                </Label>
-                <LanguageCombobox kreyolOnly />
-              </div>
-
-              <div className="grid grid-cols-5 items-center gap-4">
-                <Label htmlFor="nature" className="text-left">
-                  Nature
-                </Label>
-                <NatureCombobox />
-              </div>
-
-              <div className="grid grid-cols-5 items-center gap-4">
-
-                <Label htmlFor='meaning' className='text-left'>
-                  Définition
-                </Label>
-                <div className="col-span-4"
-                >
-                  <div className="grid grid-cols-4 items-center gap-1">
-
-                    <LanguageCombobox />
-                    <div className="col-span-3 flex w-full items-center space-x-2">
-                      <Input
-                        type="text"
-                        name="meaning"
-                        placeholder="Entrez la définition de l'entrée"
-                      />
-                      <Button size='icon' variant='outline' className='w-8 h-8'>
-                        <FeatherIcon iconName='plus' />
-                      </Button>
-
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="grid grid-cols-5 items-center gap-4">
-                <Label htmlFor="usage" className="text-left">
-                  Usage
-                </Label>
-                <Input
-                  className="col-span-4"
-                  type="text"
-                  name="usage"
-                  placeholder={`entrez une phrase d'usage avec ${entry}`}
-                />
-              </div>
-
-              <div className="grid grid-cols-5 items-center gap-4">
-                <Label htmlFor="synonyms" className="text-left">
-                  Synonymes
-                </Label>
-                <Input
-                  className="col-span-4"
-                  type="text"
-                  name="synonyms"
-                  placeholder={`entrez les synonymes de ${entry}`}
-                />
-              </div>
-
-
-            </div>
+            <AddEntry entry={entry} />
           </div>
           <div className="above-article flex flex-row"></div>
         </div>
