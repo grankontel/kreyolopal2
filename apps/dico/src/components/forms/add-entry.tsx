6c0a7fee52bd22b2a18d92627ae031a73487b9c7
Thierry Malo <thierry.malo@ringover.com>
2024-04-19 22:02:12 +0200

    enhanced state

--- apps/dico/src/components/forms/add-entry.tsx
+++ apps/dico/src/components/forms/add-entry.tsx
@@ -6,26 +6,57 @@ import { Tags } from '@/components/tags'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Quote, KreyolLanguage, Meaning } from '@kreyolopal/domain'
+import type { Nature } from '@kreyolopal/domain'
+import { useState } from 'react'
+
+export interface SubmitEntry {
+  entry: string
+  variations: string[]
+  definitions: Array<SubmitDefinition>
+}
+
+export interface SubmitDefinition {
+  kreyol: KreyolLanguage
+  nature: Nature[]
+  meaning: Meaning
+  usage: string[]
+  synonyms: string[]
+  confer: string[]
+  quotes: Quote[]
+}
+
 
 export const AddEntry = ({ entry }: { entry: string }) => {
-  const synonyms: string[] = []
-  const confer: string[] = []
+  const [variations, setVariations] = useState<string[]>([])
+  const [kreyol, setKreyol] = useState<KreyolLanguage>('gp')
+  const [nature, setNature] = useState<Nature>('nom')
+  const [meaning, setMeaning] = useState<Meaning>({})
+  const [usage, setUsage] = useState<string[]>([''])
+  const [synonyms, setSynonyms] = useState<string[]>([])
+  const [confer, setConfer] = useState<string[]>([])
+  const [quotes, setQuotes] = useState<Quote[]>([])
+
+  const var_regex = /^([a-z]([a-z-]|é|è|ò|à|)*,?)*$/gm
+  const entry_regex = /^[a-z]([a-z-]|é|è|ò|à|)*$/gm
 
   function removeConferAtIndex(index: number): void {
-    confer.splice(index, 1)
+    setConfer(confer.filter((_, i) => i !== index))
   }
 
   function addConfer(tag: string): boolean {
-    confer.push(tag)
+    // make sure the word exists
+    setConfer([...confer, tag])
     return true
   }
 
   function removeSynonymAtIndex(index: number): void {
-    synonyms.splice(index, 1)
+    setSynonyms(synonyms.filter((_, i) => i !== index))
   }
 
   function addSynonym(tag: string): boolean {
-    synonyms.push(tag)
+    // make sure the word exists
+    setSynonyms([...synonyms, tag])
     return true
   }
 
@@ -39,6 +70,13 @@ export const AddEntry = ({ entry }: { entry: string }) => {
           className="col-span-4"
           type="text"
           name="variations"
+          value={variations.join(',')}
+          onChange={(e) => {
+            if (var_regex.test(e.target.value)) {
+              setVariations(e.target.value.split(','))
+            }
+          }
+          }
           placeholder={`entrez les différentes façon d'écrire ${entry}`}
         />
       </div>
@@ -47,14 +85,14 @@ export const AddEntry = ({ entry }: { entry: string }) => {
         <Label htmlFor="kreyol" className="text-left">
           Kreyol
         </Label>
-        <LanguageCombobox kreyolOnly />
+        <LanguageCombobox kreyolOnly value={kreyol} onChange={setKreyol} />
       </div>
 
       <div className="grid grid-cols-5 items-center gap-4">
         <Label htmlFor="nature" className="text-left">
           Nature
         </Label>
-        <NatureCombobox />
+        <NatureCombobox value={nature} onChange={setNature} />
       </div>
 
       <div className="grid grid-cols-5 items-center gap-4">
@@ -78,21 +116,34 @@ export const AddEntry = ({ entry }: { entry: string }) => {
         </div>
       </div>
 
-      <div className="grid grid-cols-5 items-center gap-4">
-        <Label htmlFor="usage" className="text-left">
-          Usage
-        </Label>{' '}
-        <div className="col-span-4 flex w-full items-center space-x-2">
-          <Input
-            type="text"
-            name="usage"
-            placeholder={`entrez une phrase d'usage avec ${entry}`}
-          />
-          <Button size="icon" variant="outline" className="h-8 w-8">
-            <FeatherIcon iconName="plus" />
-          </Button>
+      {usage.map((item, index) => (
+        <div key={`usage-${index}`} className="grid grid-cols-5 items-center gap-4">
+          <Label htmlFor="usage" className="text-left">
+            Usage
+          </Label>{' '}
+          <div className="col-span-4 flex w-full items-center space-x-2">
+            <Input
+              type="text"
+              name="usage"
+              placeholder={`entrez une phrase d'usage avec ${entry}`}
+              value={usage[index]}
+              onChange={(e) => {
+                setUsage([...usage.slice(0, index), e.target.value, ...usage.slice(index + 1)])
+              }}
+            />
+            {(index !== usage.length - 1) ? null : (
+              <Button size="icon" variant="outline" className="h-8 w-8"
+                onClick={() => {
+                  if (usage[index].length > 3)
+                    setUsage([...usage, ''])
+                }
+                }
+              >
+                <FeatherIcon iconName="plus" />
+              </Button>)}
+          </div>
         </div>
-      </div>
+      ))}
 
       <div className="grid grid-cols-5 items-center gap-4">
         <Label htmlFor="synonyms" className="text-left">
