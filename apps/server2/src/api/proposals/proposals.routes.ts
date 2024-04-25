3960d2a9b10a92f7d92ef513ecea5873fa0278e8
Thierry Malo <thierry.malo@ringover.com>
2024-04-25 15:57:42 +0200

    correct schema

--- apps/server2/src/api/proposals/proposals.routes.ts
+++ apps/server2/src/api/proposals/proposals.routes.ts
@@ -1,11 +1,10 @@
 import { createRouter, sendBadRequest } from '#services/hono'
-import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 import handlers from './proposals.handlers'
-import { SubmitDefinitionSchema } from '@kreyolopal/domain'
+import { SubmitEntrySchema } from '@kreyolopal/domain'
 
 const routes = createRouter()
 
-routes.post('/', zValidator('json', SubmitDefinitionSchema, sendBadRequest), handlers.submitProposal)
+routes.post('/', zValidator('json', SubmitEntrySchema, sendBadRequest), handlers.submitProposal)
 
 export default routes
