d9c5c311dffc0748114edc5409c00e39f27b2b2d
Thierry Malo <thierry.malo@ringover.com>
2024-04-29 09:41:28 +0200

    moved add to lexicon component

---
+++ apps/dico/src/components/entry/add-to-lexicon.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { SingleDefinition, } from '@kreyolopal/domain'
+import FeatherIcon from '../FeatherIcon'
+import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { LexiconDropdownMenu } from '@/components/lexicons/lexicon-dropdown-menu'
+import { useMutation } from '@tanstack/react-query'
+import { useDashboard } from '@/app/dashboard/dashboard-provider'
+import { useToast } from '@/components/ui/use-toast'
+import { addEntries } from '@/queries/lexicons/add-entries'
+
+
+export const AddToLexicon = ({ definition }: { definition: SingleDefinition }) => {
+	const dash = useDashboard()
+	const { toast } = useToast()
+	const notifyer = (err: { error?: string; toString: () => string }) => {
+		toast({
+			title: 'Erreur',
+			variant: 'destructive',
+			description: err?.error || err.toString(),
+		})
+	}
+
+	const addEntry = useMutation({
+		mutationFn: ({ lexiconId }: { lexiconId: string }) => {
+			return addEntries(
+				lexiconId,
+				{
+					entry: definition.entry,
+					definitions: [
+						{
+							source: definition.source,
+							id: definition.definition_id,
+						},
+					],
+				},
+				dash?.session_id
+			)
+		},
+		onSuccess: () => {
+			toast({
+				variant: 'default',
+				description: 'Entrée ajoutée',
+			})
+		},
+		onError: (err: Error) => {
+			notifyer(err)
+		},
+	})
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger
+				className={`focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground 
+    inline-flex h-9 w-9 items-center justify-center whitespace-nowrap 
+    rounded-md border text-sm font-medium shadow-sm transition-colors 
+    focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50`}
+			>
+				<FeatherIcon iconName="chevron-right" />
+			</DropdownMenuTrigger>
+			<LexiconDropdownMenu
+				onSelect={(item) => {
+					addEntry.mutate({ lexiconId: item.id })
+				}}
+			/>
+		</DropdownMenu>
+	)
+}
--- apps/dico/src/components/entry/entry-definition.tsx
+++ apps/dico/src/components/entry/entry-definition.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 import Link from 'next/link'
 import {
   MeaningLanguage,
@@ -7,16 +7,9 @@ import {
   KreyolLanguage,
 } from '@kreyolopal/domain'
 import { hashKey } from '@/lib/utils'
-import FeatherIcon from '../FeatherIcon'
-import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { LexiconDropdownMenu } from '@/components/lexicons/lexicon-dropdown-menu'
 import { dicoUrl } from '@/lib/dicoUrl'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { useDashboard } from '@/app/dashboard/dashboard-provider'
-import { useToast } from '@/components/ui/use-toast'
-import { addEntries } from '@/queries/lexicons/add-entries'
-import { useState } from 'react'
 import { ProposalVoteButtons } from '@/components/proposal-vote-buttons'
+import { AddToLexicon } from './add-to-lexicon'
 
 interface EntryDefinitionProps {
   entry: string
@@ -25,105 +18,13 @@ interface EntryDefinitionProps {
   definition: SingleDefinition | ProposalDefinition
 }
 
-const AddToLexicon = ({ definition }: { definition: SingleDefinition }) => {
-  const dash = useDashboard()
-  const { toast } = useToast()
-  const notifyer = (err: { error?: string; toString: () => string }) => {
-    toast({
-      title: 'Erreur',
-      variant: 'destructive',
-      description: err?.error || err.toString(),
-    })
-  }
-
-  const addEntry = useMutation({
-    mutationFn: ({ lexiconId }: { lexiconId: string }) => {
-      return addEntries(
-        lexiconId,
-        {
-          entry: definition.entry,
-          definitions: [
-            {
-              source: definition.source,
-              id: definition.definition_id,
-            },
-          ],
-        },
-        dash?.session_id
-      )
-    },
-    onSuccess: () => {
-      toast({
-        variant: 'default',
-        description: 'Entrée ajoutée',
-      })
-    },
-    onError: (err: Error) => {
-      notifyer(err)
-    },
-  })
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
-        className={`focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground 
-    inline-flex h-9 w-9 items-center justify-center whitespace-nowrap 
-    rounded-md border text-sm font-medium shadow-sm transition-colors 
-    focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50`}
-      >
-        <FeatherIcon iconName="chevron-right" />
-      </DropdownMenuTrigger>
-      <LexiconDropdownMenu
-        onSelect={(item) => {
-          addEntry.mutate({ lexiconId: item.id })
-        }}
-      />
-    </DropdownMenu>
-  )
-}
 export const EntryDefinition = ({
   entry,
   kreyol,
   index,
   definition,
 }: EntryDefinitionProps) => {
-  const [lexiconId, setLexiconId] = useState<string>()
-  const dash = useDashboard()
-  const queryClient = useQueryClient()
-  const { toast } = useToast()
-  const notifyer = (err: { error?: string; toString: () => string }) => {
-    toast({
-      title: 'Erreur',
-      variant: 'destructive',
-      description: err?.error || err.toString(),
-    })
-  }
 
-  const addEntry = useMutation({
-    mutationFn: () => {
-      return addEntries(
-        lexiconId as string,
-        {
-          entry: entry,
-          definitions: [
-            {
-              source: (definition as SingleDefinition).source,
-              id: definition.definition_id,
-            },
-          ],
-        },
-        dash?.session_id
-      )
-    },
-    onSuccess: () => {
-      toast({
-        variant: 'default',
-        description: 'Entrée ajoutée',
-      })
-    },
-    onError: (err: Error) => {
-      notifyer(err)
-    },
-  })
   const nature = definition.nature.join(', ')
   const subnature = definition.subnature?.length
     ? definition.subnature.join(', ')
