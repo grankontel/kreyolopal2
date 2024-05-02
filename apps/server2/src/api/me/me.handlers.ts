7bf0fa6b6eb87e646b9707015e8d09adbdb29113
Thierry Malo <thierry.malo@ringover.com>
2024-05-02 17:07:30 +0200

    added updatepwd route

--- apps/server2/src/api/me/me.handlers.ts
+++ apps/server2/src/api/me/me.handlers.ts
@@ -1,4 +1,6 @@
 import type { Context } from 'hono'
+import type { PoolClient } from 'pg'
+import { argon2 } from '#utils/argon'
 
 /*
 
@@ -34,4 +36,52 @@ const getUserInfo = async function (c: Context) {
   return c.json(response, 200)
 }
 
-export default { getUserInfo }
+const updatePassword = async function (c: Context) {
+  const logger = c.get('logger')
+  const pgPool = c.get('pgPool')
+  const user = c.get('user')
+
+  logger.info('updatePassword')
+
+  if (!user) {
+    logger.debug('user not logged in')
+    return c.json({ error: 'You are not logged in.' }, 403)
+  }
+
+  const { old_password, new_password } = c.req.valid('json')
+  const client: PoolClient = await pgPool.connect()
+  try {
+    const result = await client.query(
+      'SELECT username, password FROM auth_user WHERE id = $1',
+      [user.id]
+    )
+
+    const isValid = await argon2.verify(old_password, result.rows[0].password)
+    if (!isValid) {
+      logger.warn('old password is not valid')
+      return c.json(
+        {
+          status: 'error',
+          error: 'Old password is not valid',
+        },
+        400
+      )
+    }
+    const hashedPassword = await argon2.hash(new_password)
+
+    await client
+    .query('UPDATE auth_user SET password= $2 WHERE id = $1 ', [
+      user.id, hashedPassword
+    ])
+
+    return c.json({ status: 'success' }, 200)
+
+  } catch (_error) {
+    logger.error('updatePassword Exception', _error)
+    return c.json({ status: 'error', error: [_error] }, 500)
+  } finally {
+    client.release()
+  }
+}
+
+export default { getUserInfo, updatePassword }
--- apps/server2/src/api/me/me.routes.ts
+++ apps/server2/src/api/me/me.routes.ts
@@ -1,9 +1,23 @@
 import { createRouter, sendBadRequest } from '#services/hono'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
 import handlers from './me.handlers'
 
 const routes = createRouter()
 
+const postUpdatePasswordSchema = z.object({
+  old_password: z.string().min(3),
+  new_password: z.string().min(10).regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{10,}$/),
+	verification: z.string().min(10)  
+}).refine((data) => data.new_password === data.verification, {
+	message: 'Password confirmation does not match password',
+	path: ['verification'], // path of error
+})
+
 // get specific word
 routes.get('/', handlers.getUserInfo)
 
+routes.post('/updatepwd',
+zValidator('json', postUpdatePasswordSchema, sendBadRequest),
+handlers.updatePassword)
 export default routes
