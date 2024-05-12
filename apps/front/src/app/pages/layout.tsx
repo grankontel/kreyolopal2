66ea7459f24fc5e74cebfe8c191f6f7e73bd1482
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-05-12 19:53:45 +0200

    add layout

---
+++ apps/front/src/app/pages/layout.tsx
@@ -0,0 +1,12 @@
+
+export default function Layout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode
+  }>) {
+    return (
+        <article className="prose">
+            {children}
+        </article>
+    )
+  }
