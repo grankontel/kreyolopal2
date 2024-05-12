a5e4db91f00dd5afd0bda61ecc870ef75f47c7ee
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-05-12 20:02:10 +0200

    enhanced layout

--- apps/front/src/app/pages/layout.tsx
+++ apps/front/src/app/pages/layout.tsx
@@ -1,12 +1,16 @@
+import HomeFooter from '../_components/home-footer'
+import HomeHeader from '../_components/home-header'
 
 export default function Layout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode
-  }>) {
-    return (
-        <article className="prose">
-            {children}
-        </article>
-    )
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <main className="flex min-h-[100dvh] flex-col">
+      <HomeHeader />
+      <article className="prose ">{children}</article>
+      <HomeFooter />
+    </main>
+  )
+}
