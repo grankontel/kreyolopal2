7d1cea4ddacd6d2795f33456548f43dbc82c0d1d
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-05-14 12:38:46 +0200

    enhanced display

--- apps/front/src/app/pages/layout.tsx
+++ apps/front/src/app/pages/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({
   children: React.ReactNode
 }>) {
   return (
-    <main className="flex min-h-[100dvh] flex-col">
+    <main className="flex min-h-[100dvh] flex-col py-12 md:py-24 lg:py-32 xl:py-48">
       <HomeHeader />
       <article className="prose prose-lg dark:prose-invert max-w-none m-6 w-3/4">{children}</article>
       <HomeFooter />
