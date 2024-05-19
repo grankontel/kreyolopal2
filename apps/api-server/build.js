959fdd80c7e16985a67a7b2535fe4ca3709bf402
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-05-19 11:32:41 +0200

    build cli

--- apps/api-server/build.js
+++ apps/api-server/build.js
@@ -8,6 +8,10 @@ pdata.main = 'server.js'
 pdata.scripts = {
   start: "NODE_ENV='production' node server.js",
 }
+pdata.bin = {
+  "cli": "./cli.js"
+}
+
 pdata.dependencies = pdata.peerDependencies
 delete pdata.peerDependencies
 delete pdata.devDependencies
@@ -15,6 +19,7 @@ delete pdata.imports
 
 const nodenv = process.env.NODE_ENV || 'development'
 
+console.log('Building server ...')
 build({
   entryPoints: ['src/index.ts'],
   outfile: 'dist/server.js',
@@ -40,3 +45,33 @@ build({
     console.log('⚡ Done')
   })
   .catch(() => process.exit(1))
+
+  console.log('Building cli ...')
+  build({
+    entryPoints: ['src/cli.ts'],
+    outfile: 'dist/_cli.js',
+    bundle: true,
+    minify: process.env.NODE_ENV === 'production',
+    platform: 'node',
+    loader: { '.ts': 'ts' },
+    // packages: 'external',
+    sourcemap: nodenv === 'development',
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': `"${nodenv}"`,
+      }),
+    ],
+  })
+  .then(() => {
+    // prettier-ignore
+    fs.readFile('./dist/_cli.js', {encoding: 'utf-8'}, function(err,data){
+      if (!err) {
+          fs.writeFileSync('./dist/cli.js', '#!/usr/bin/env node\n\n' + data)
+      } else {
+          console.log(err);
+      }
+  });    
+    console.log('⚡ Done')
+  })
+    .catch(() => process.exit(1))
+  
--- apps/api-server/src/cli.ts
+++ apps/api-server/src/cli.ts
@@ -1,15 +1,29 @@
-import { setDefaultPermissions } from "#services/def_perm";
-import { program } from "commander";
+import { setDefaultPermissions, setDefaultRoles } from '#services/def_perm'
+import { Command } from 'commander'
+
+const program = new Command()
+
+const perms = new Command('perms')
+  .command('perms')
+  .description('Set default permissions')
+  .action(async () => {
+    await setDefaultPermissions()
+    console.log('Default permissions inserted')
+  })
+
+const roles = new Command('roles')
+  .command('roles')
+  .description('Set default roles')
+  .action(async () => {
+    await setDefaultRoles()
+    console.log('Default roles inserted')
+  })
 
 program
-	.name("kreyolopal-cli")
-	.version("1.0.0", '-v, --version')
-	.description("Kreyolopal API Server")
-	.command('perms')
-	.description('Set default permissions')
-	.action(async () => {
-		await setDefaultPermissions()
-		console.log('Default permissions inserted')
-	})
+  .name('kreyolopal-cli')
+  .version('1.0.0', '-v, --version')
+  .description('Kreyolopal API Server')
+  .addCommand(perms)
+  .addCommand(roles)
 
-program.parse(process.argv);
+program.parse(process.argv)
