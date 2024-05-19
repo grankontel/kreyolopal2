5a1fc8f39c2b64fbf07e30b964b4c5af7e26885a
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-05-19 19:16:01 +0200

    alias management

--- tools/dico2json2.js
+++ tools/dico2json2.js
@@ -9,6 +9,10 @@ const DEFINITION_RANK_FIELD = 2
 const NATURE_FIELD = 3
 const PREFIX_FIELD = 4
 const SUFFIX_FIELD = 5
+
+// const PREFIX_FIELD = 3
+// const SUFFIX_FIELD = 4
+// const NATURE_FIELD = 5
 const AS_IN_FIELD = 6
 const MEANING_FR_FIELD = 7
 const USAGE_FIELD = 8
@@ -18,8 +22,8 @@ const CONFER_FIELD = 10
 const fname = argv[2]
 
 if (!fname?.length > 0) {
-	console.error('No filename provided')
-	process.exit(1)
+  console.error('No filename provided')
+  process.exit(1)
 }
 
 const csvfile = path.resolve(fname)
@@ -29,72 +33,72 @@ fs.mkdirSync(path.dirname(datafile), { recursive: true })
 const liner = new lineByLine(csvfile)
 
 const natures = [
-	'adjectif',
-	'adverbe',
-	'article',
-	'conjonction',
-	'exclamation',
-	'expression',
-	'interjection',
-	'locution',
-	'nom',
-	'nom propre',
-	'numeral',
-	'onomatopée',
-	'particule',
-	'préfixe',
-	'préposition',
-	'pronom',
-	'suffixe',
-	'verbe',
+  'adjectif',
+  'adverbe',
+  'article',
+  'conjonction',
+  'exclamation',
+  'expression',
+  'interjection',
+  'locution',
+  'nom',
+  'nom propre',
+  'numeral',
+  'onomatopée',
+  'particule',
+  'préfixe',
+  'préposition',
+  'pronom',
+  'suffixe',
+  'verbe',
 ]
 
 const subnatures = [
-	'adverbiale',
-	'aspectuelle',
-	'défini',
-	'grammaticale',
-	'indéfini',
-	'interrogatif',
-	'négation',
-	'numéral',
-	'modale',
-	'personnel',
-	'impersonnel',
-	'possessif',
-	'postposé',
-	'pronominale',
-	'subordination',
-	'sériel',
+  'adverbiale',
+  'aspectuelle',
+  'défini',
+  'grammaticale',
+  'indéfini',
+  'interrogatif',
+  'négation',
+  'numéral',
+  'modale',
+  'personnel',
+  'impersonnel',
+  'possessif',
+  'postposé',
+  'pronominale',
+  'subordination',
+  'sériel',
 ]
 
 function cleanNature(nat) {
-	if (natures.includes(nat)) return nat
+  if (natures.includes(nat)) return nat
 
-	if (['vebre', 'verb'].includes(nat)) return 'verbe'
-	if (['noun', 'nm', 'nomn', 'non'].includes(nat)) return 'nom'
-	if (['adjverbe'].includes(nat)) return 'adverbe'
-	if (['adjective', 'adejctif', 'advectif', 'adjecif'].includes(nat)) return 'adjectif'
-	if (['pronoun'].includes(nat)) return 'pronom'
-	if (['prefixe'].includes(nat)) return 'préfixe'
+  if (['vebre', 'verb'].includes(nat)) return 'verbe'
+  if (['noun', 'nm', 'nomn', 'non'].includes(nat)) return 'nom'
+  if (['adjverbe'].includes(nat)) return 'adverbe'
+  if (['adjective', 'adejctif', 'advectif', 'adjecif'].includes(nat)) return 'adjectif'
+  if (['pronoun'].includes(nat)) return 'pronom'
+  if (['prefixe'].includes(nat)) return 'préfixe'
 
-	return nat
+  return nat
 }
 
 function cleanSubNature(nat) {
-	if (subnatures.includes(nat)) return nat
+  if (subnatures.includes(nat)) return nat
 
-	if (['posséssif'].includes(nat)) return 'possessif'
+  if (['posséssif'].includes(nat)) return 'possessif'
 
-	return nat
+  return nat
 }
 
 function removeAccents(str) {
-	return str
-		.replaceAll('à', 'a_')
-		.replaceAll('ò', 'o_')
-		.replaceAll('é', 'e_')
-		.replaceAll('è', 'e_')
+  return str
+    .replaceAll('à', 'a_')
+    .replaceAll('ò', 'o_')
+    .replaceAll('é', 'e_')
+    .replaceAll('è', 'e_')
 }
 
 let line
@@ -109,34 +113,34 @@ let rank = 0
 let radix = ''
 let newEntry = {}
 while ((line = liner.next())) {
-	// console.log('Line ' + lineNumber)
-	const values = line.toString('utf8').split(';')
-	let ent = values[ENTRY_FIELD].replaceAll(' ', ' ').trim()
-	if (ent === '' || values[VARIATIONS_FIELD] === 'variations') {
-		lineNumber++
-		continue
-	}
+  // console.log('Line ' + lineNumber)
+  const values = line.toString('utf8').split(';')
+  let ent = values[ENTRY_FIELD].replaceAll(' ', ' ').trim()
+  if (ent === '' || values[VARIATIONS_FIELD] === 'variations') {
+    lineNumber++
+    continue
+  }
 
-	if (entry !== ent) {
-		const vari = values[VARIATIONS_FIELD].split('/').map((x) => x.trim())
+  if (entry !== ent) {
+    const vari = values[VARIATIONS_FIELD].split('/').map((x) => x.trim()).filter((x) => x.length > 0)
 
-		//find aliases
-		const alias_entries = vari.filter((x) => x != ent)
+    //find aliases
+    const alias_entries = vari.filter((x) => x != ent)
 
-		// new entry
-		let newEntry = {
-			entry: ent,
-			docType: 'entry',
-			// make sure entry is the first variations
-			variations: [ent, ...alias_entries],
-		}
-		existingEntries.push(newEntry)
-		entries.push(newEntry)
-		entry = ent
-		rank = 0
-		radix = removeAccents(ent) // Buffer.from(ent, 'utf-8').toString('ascii')
+    // new entry
+    let newEntry = {
+      entry: ent,
+      docType: 'entry',
+      // make sure entry is the first variations
+      variations: [ent, ...alias_entries],
+    }
+    existingEntries.push(newEntry)
+    entries.push(newEntry)
+    entry = ent
+    rank = 0
+    radix = removeAccents(ent) // Buffer.from(ent, 'utf-8').toString('ascii')
 
-		const aliases = alias_entries.map((x) => {
+    /* 		const aliases = alias_entries.map((x) => {
 			return { 
 				entry: x,
 				docType: 'entry',
@@ -145,86 +149,89 @@ while ((line = liner.next())) {
 				aliasOf: newEntry.entry
 				}
 		})
-		entries.push(...aliases)
-	}
+		entries.push(...aliases) */
+  }
 
-	// add definition
-	let definition = {
-		entry: ent,
-		docType: 'definition',
-		definition_id: `${radix}_${rank}`,
-		kreyol: 'gp',
-		rank: rank++,
-	}
+  // add definition
+  let definition = {
+    entry: ent,
+    docType: 'definition',
+    definition_id: `${radix}_${rank}`,
+    kreyol: 'gp',
+    rank: rank++,
+  }
 
-	/*
+  /*
 const PREFIX_FIELD = 4
 const SUFFIX_FIELD = 5
 const AS_IN_FIELD = 6
 */
 
-	if (values[PREFIX_FIELD].trim().length > 0) {
-		definition.prefix = values[PREFIX_FIELD].trim()
-	}
+  if (values[PREFIX_FIELD].trim().length > 0) {
+    definition.prefix = values[PREFIX_FIELD].trim()
+  }
 
-	if (values[SUFFIX_FIELD].trim().length > 0) {
-		definition.suffix = values[SUFFIX_FIELD].trim()
-	}
+  if (values[SUFFIX_FIELD].trim().length > 0) {
+    definition.suffix = values[SUFFIX_FIELD].trim()
+  }
 
-	if (values[AS_IN_FIELD].trim().length > 0) {
-		definition.asIn = values[AS_IN_FIELD].trim()
-	}
+  if (values[AS_IN_FIELD].trim().length > 0) {
+    definition.asIn = values[AS_IN_FIELD].trim()
+  }
 
+  try {
+    if (values[NATURE_FIELD].trim() == 'nom propre') {
+      definition.nature = ['nom propre']
+    } else {
+      let cleanats = values[NATURE_FIELD].trim().replace(' de ', ' ')
+      let nats = cleanats.split(' ')
 
-	try {
-		if (values[NATURE_FIELD].trim() == 'nom propre') {
-			definition.nature = ['nom propre']
-		} else {
-			let cleanats = values[NATURE_FIELD].trim().replace(' de ', ' ')
-			let nats = cleanats.split(' ')
+      let nat = cleanNature(nats[0])
+      definition.nature = [nat]
+      if (!natures.includes(nat)) {
+        console.log(`\t entry : ${entry}, line : ${lineNumber} ; ${nat} inconnu`)
+        errors++
+      } else if (nats[1]?.length > 0) {
+        let subnature = cleanSubNature(nats[1])
 
-			let nat = cleanNature(nats[0])
-			definition.nature = [nat]
-			if (!natures.includes(nat)) {
-				console.log(`\t entry : ${entry}, line : ${lineNumber} ; ${nat} inconnu`)
-				errors++
-			} else if (nats[1]?.length > 0) {
-				let subnature = cleanSubNature(nats[1])
+        if (subnatures.includes(subnature)) {
+          definition.subnature = []
+          definition.subnature.push([nat, subnature].join(' '))
+        } else {
+          console.log(
+            `\t entry : ${entry}, line : ${lineNumber} ; ${values[NATURE_FIELD].trim()} inconnu`
+          )
+          errors++
+        }
+      }
+    }
+  } catch (e) {
+    console.error(`error at line ${lineNumber}`)
+    console.log(e)
+    process.exit(1)
+  }
 
-				if (subnatures.includes(subnature)) {
-					definition.subnature = []
-					definition.subnature.push([nat, subnature].join(' '))
-				} else {
-					console.log(
-						`\t entry : ${entry}, line : ${lineNumber} ; ${values[NATURE_FIELD].trim()} inconnu`
-					)
-					errors++
-				}
-			}
-		}
-	} catch (e) {
-		console.error(`error at line ${lineNumber}`)
-		console.log(e)
-		process.exit(1)
-	}
+  definition.meaning = {}
+  definition.meaning.gp = ''
+  definition.meaning.fr = values[MEANING_FR_FIELD]?.trim().replaceAll(' ', '')
 
-	definition.meaning = {}
-	definition.meaning.gp = ''
-	definition.meaning.fr = values[MEANING_FR_FIELD]?.trim().replaceAll(' ', '')
-	definition.usage = values[USAGE_FIELD].split('/')
-		.map((x) => x.trim().replaceAll(' ', ''))
-		.filter((y) => y.length > 0)
-	definition.synonyms = values[SYNONYMS_FIELD].split(',')
-		.map((x) => x.trim())
-		.filter((y) => y.length > 0)
-	definition.confer = values[CONFER_FIELD]?.split('/')
-		.map((x) => x.trim())
-		.filter((y) => y.length > 0)
-		.filter((s) => !newEntry.variations?.includes(s) && !definition.synonyms.includes(s))
-	definition.quotes = []
+  definition.usage = values[USAGE_FIELD].split('/')
+    .map((x) => x.trim().replaceAll(' ', ''))
+    .filter((y) => y.length > 0)
 
-	entries.push(definition)
-	lineNumber++
+  definition.synonyms = values[SYNONYMS_FIELD].split(',')
+    .map((x) => x.trim())
+    .filter((y) => y.length > 0)
+
+  definition.confer = values[CONFER_FIELD]?.split('/')
+    .map((x) => x.trim())
+    .filter((y) => y.length > 0)
+    .filter((s) => !newEntry.variations?.includes(s) && !definition.synonyms.includes(s))
+    
+  definition.quotes = []
+
+  entries.push(definition)
+  lineNumber++
 }
 
 console.log('end of line reached')
@@ -235,7 +242,7 @@ console.log(`${errors} errors`)
 for (let index = 0; index < entries.length; index++) {
   const element = entries[index]
   if (element.docType == 'entry') 
-		continue;
+    continue
 
   if (element.synonyms.length > 0) {
     const realSynonyms = element.synonyms.map((syn) => {
@@ -251,6 +258,45 @@ for (let index = 0; index < entries.length; index++) {
     element.confer = realConfer.filter((x) => x !== undefined)
   }
 }
+
+console.log('checked synonyms and confers')
+
+// add aliases
+const aliasArray = []
+for (let index = 0; index < entries.length; index++) {
+  const anEntry = entries[index]
+  if (anEntry.docType === 'definition') 
+    continue
+
+  const alias_entries = anEntry.variations
+  .filter((x) => x != anEntry.entry)
+
+  alias_entries.forEach((x) => {
+    if (aliasArray.findIndex((y) => y.docType=== 'entry'  && y.entry === x) != -1) 
+      return
+
+    if (entries.findIndex((y) => y.docType=== 'entry'  && y.entry === x) != -1) 
+      return
+
+    const alias = {
+      entry: x,
+      docType: 'entry',
+      // make sure entry is the first variations
+      variations: anEntry.variations,
+      aliasOf: anEntry.entry,
+    }
+    aliasArray.push(alias)
+  })
+}
+
+entries.push(...aliasArray)
+
+console.log(`${entries.length} entries`)
+
+entries.sort((a, b) => {
+  return a.entry.localeCompare(b.entry)
+})
+
 // write JSON string to a file
 fs.writeFile(datafile, JSON.stringify(entries, null, 2), (err) => {
   if (err) {
