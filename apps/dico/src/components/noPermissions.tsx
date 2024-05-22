d2269167307db9b711eaa1c277d7488f9a9dacf9
Thierry Malo <thierry.malo@ringover.com>
2024-05-22 08:18:44 +0200

    enhanced no permission alert

--- apps/dico/src/components/noPermissions.tsx
+++ apps/dico/src/components/noPermissions.tsx
@@ -1,16 +1,30 @@
+'use client'
+
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
-
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+import FeatherIcon from './FeatherIcon'
 
 const NoPermissions = () => {
-	const router = useRouter()
+  const router = useRouter()
   return (
     <div>
-      <h2>Forbidden</h2>
-      <p>Vos permissions sont insuffisantes pour accéder à cette page</p>
-			<Button onClick={() => router.back()}>
-          Retour
-        </Button>
+      <Alert variant={'destructive'}>
+        <div className='flex flex-row gap-2 py-2'>
+          <FeatherIcon iconName='alert-circle' className="h-4 w-4" />
+          <AlertTitle>Forbidden !</AlertTitle>
+        </div>
+        <AlertDescription>
+          <p>Vos permissions sont insuffisantes pour accéder à cette page.</p>
+          <Button className='mt-2' onClick={() => router.back()}>
+            Retour
+          </Button>
+        </AlertDescription>
+      </Alert>
 
     </div>
   )
