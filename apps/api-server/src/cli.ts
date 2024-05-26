55ae660048de73b5d257256664401c7781c448ed
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-05-26 11:28:41 +0200

    added migrate command

--- apps/api-server/src/cli.ts
+++ apps/api-server/src/cli.ts
@@ -1,4 +1,5 @@
 import { setDefaultPermissions, setDefaultRoles } from '#services/def_perm'
+import { migratePostgres } from '#services/migrate'
 import { Command } from 'commander'
 
 const program = new Command()
@@ -19,10 +20,19 @@ const roles = new Command('roles')
     console.log('Default roles inserted')
   })
 
+const migrate = new Command('migrate')
+  .command('migrate')
+  .description('migrate to most up to date schema')
+  .requiredOption('-d, --dir <directory>', 'Migrations directory')
+  .action(async (options) => {
+    await migratePostgres(options.dir)
+    console.log('migrated')
+  })
 program
   .name('kreyolopal-cli')
   .version('1.0.0', '-v, --version')
   .description('Kreyolopal API Server')
+  .addCommand(migrate)
   .addCommand(perms)
   .addCommand(roles)
 
