27534d375577c5efbc7b297dd501cdfb1edd8add
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2024-05-26 11:28:58 +0200

    improve build

--- apps/api-server/build.js
+++ apps/api-server/build.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { build } = require('esbuild')
 const { replace } = require('esbuild-plugin-replace')
-const fs = require('node:fs')
+const fs = require('node:fs').promises
 var pdata = require('./package.json')
 
 pdata.main = 'server.js'
@@ -9,7 +9,7 @@ pdata.scripts = {
   start: "NODE_ENV='production' node server.js",
 }
 pdata.bin = {
-  "cli": "./cli.js"
+  cli: './cli.js',
 }
 
 pdata.dependencies = pdata.peerDependencies
@@ -20,7 +20,7 @@ delete pdata.imports
 const nodenv = process.env.NODE_ENV || 'development'
 
 console.log('Building server ...')
-build({
+const buildServer = build({
   entryPoints: ['src/index.ts'],
   outfile: 'dist/server.js',
   bundle: true,
@@ -36,9 +36,9 @@ build({
     }),
   ],
 })
-  .then(() => {
+  .then(async () => {
     // prettier-ignore
-    fs.writeFileSync(
+    await fs.writeFile(
       './dist/package.json',
       JSON.stringify(pdata, null, '  ')
     )
@@ -46,32 +46,38 @@ build({
   })
   .catch(() => process.exit(1))
 
-  console.log('Building cli ...')
-  build({
-    entryPoints: ['src/cli.ts'],
-    outfile: 'dist/_cli.js',
-    bundle: true,
-    minify: process.env.NODE_ENV === 'production',
-    platform: 'node',
-    loader: { '.ts': 'ts' },
-    // packages: 'external',
-    sourcemap: nodenv === 'development',
-    plugins: [
-      replace({
-        'process.env.NODE_ENV': `"${nodenv}"`,
-      }),
-    ],
-  })
-  .then(() => {
+console.log('Building cli ...')
+const buildCli = build({
+  entryPoints: ['src/cli.ts'],
+  outfile: 'dist/_cli.js',
+  bundle: true,
+  minify: process.env.NODE_ENV === 'production',
+  platform: 'node',
+  loader: { '.ts': 'ts' },
+  // packages: 'external',
+  sourcemap: nodenv === 'development',
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': `"${nodenv}"`,
+    }),
+  ],
+})
+  .then(async () => {
     // prettier-ignore
-    fs.readFile('./dist/_cli.js', {encoding: 'utf-8'}, function(err,data){
-      if (!err) {
-          fs.writeFileSync('./dist/cli.js', '#!/usr/bin/env node\n\n' + data)
-      } else {
-          console.log(err);
-      }
-  });    
+    fs.cp('./src/db/migrations', './dist/migrations', {recursive: true})
+    .catch((err) => console.log(err))
+
+    await  fs.readFile('./dist/_cli.js', { encoding: 'utf-8' })
+    .then((data) => {
+      console.log('create cli.js')
+      return fs.writeFile('./dist/cli.js', '#!/usr/bin/env node\n\n' + data)
+      .then(() => {
+        console.log('chmod')
+        return fs.chmod('./dist/cli.js', 0o744).catch((err) => console.log(err))
+      })
+    })
     console.log('⚡ Done')
   })
-    .catch(() => process.exit(1))
-  
+  .catch(() => process.exit(1))
+
+  Promise.all([buildServer, buildCli])
